6fc9e2f6aafcc6860ecb3e0a20a6cdc9a441d20e
tozawahiroya <115676510+tozawahiroya@users.noreply.github.com>
2023-01-30 21:08:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
-from __future__ import division
-from google.cloud import speech
-
-import os
-import pandas as pd
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'tech0-step3-te-bd23bed77076.json'
-
-import re
-import sys
-
-import pyaudio
-from six.moves import queue
-
 import streamlit as st
-import time
-
-global total_words
-total_words = ""
 
 st.button('Submit！')
 st.markdown('↑　現状は押すと初期画面に戻る。MVPとしては下記にFeedback画面が表示されるイメージ')
