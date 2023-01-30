010f30a7ec65864eca5ca7a5efcc99c5957f03b4
tozawahiroya <115676510+tozawahiroya@users.noreply.github.com>
2023-01-30 21:12:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-import streamlit as st
+from __future__ import division
+
+import os
+import pandas as pd
+
+import re
+import sys
 
 import pyaudio
+from six.moves import queue
+
+import streamlit as st
+import time
 
 st.button('Submit！')
 st.markdown('↑　現状は押すと初期画面に戻る。MVPとしては下記にFeedback画面が表示されるイメージ')
