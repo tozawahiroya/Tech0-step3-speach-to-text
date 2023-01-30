c2c3d9567c1b8a1f66cf5b44f3f261f252f6e1ac
tozawahiroya <115676510+tozawahiroya@users.noreply.github.com>
2023-01-30 21:13:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import time
 
 st.button('Submit！')
 st.markdown('↑　現状は押すと初期画面に戻る。MVPとしては下記にFeedback画面が表示されるイメージ')
-st.markdown('↑　現状は押すと初期画面に戻る。MVPとしては下記にFeedback画面が表示されるイメージ')
+st.button('Submit！')
 
 
 
